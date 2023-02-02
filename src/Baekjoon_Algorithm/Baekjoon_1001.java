18c1a99661742d51b5be301c2b24fd12133908f4
dongh810 <baek0810@naver.com>
2023-02-03 02:17:43 +0900

    A-B

---
+++ src/Baekjoon_Algorithm/Baekjoon_1001.java
@@ -0,0 +1,13 @@
+package Baekjoon_Algorithm;
+
+import java.util.Scanner;
+
+public class Baekjoon_1001 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int C = A-B;
+        System.out.println(C);
+    }
+}
