aa91d9fe27742804885a0d560b5b8d049112ed2e
dongh810 <baek0810@naver.com>
2023-02-05 04:27:01 +0900

    A,B 비교

---
+++ src/Baekjoon_Algorithm/Baekjoon_1330.java
@@ -0,0 +1,19 @@
+package Baekjoon_Algorithm;
+
+import java.util.Scanner;
+
+public class Baekjoon_1330 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        if (A>B){
+            System.out.println(">");
+        }
+        else if (A<B){
+            System.out.println("<");
+        }
+        else
+            System.out.println("==");
+    }
+}
