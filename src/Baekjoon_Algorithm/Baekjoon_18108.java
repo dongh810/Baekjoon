0df67e0909175195a55216684bb9e3bd6c8ba56e
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-02-27 12:14:09 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ src/Baekjoon_Algorithm/Baekjoon_18108.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjoon_18108 {
+    public Baekjoon_18108() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        int a = Integer.parseInt(tmp);
+        System.out.println(a - 543);
+    }
+}
