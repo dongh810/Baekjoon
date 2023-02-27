4805e41e1c68e6a9ba556ad012e0d0a2ca561841
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-02-27 12:22:36 +0900

    고양이

---
+++ src/Baekjoon_Algorithm/Baekjoon_10171.java
@@ -0,0 +1,11 @@
+package Baekjoon_Algorithm;
+
+public class Baekjoon_10171 {
+    public static void main(String args[]){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
