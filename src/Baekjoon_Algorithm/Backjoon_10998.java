8712ab7dcd39ed463fe77ce783eb7501ae5f4b4c
dongh810 <105986200+dongh810@users.noreply.github.com>
2023-07-12 03:06:05 +0900

    A×B

---
+++ src/Baekjoon_Algorithm/Backjoon_10998.java
@@ -0,0 +1,14 @@
+package Baekjoon_Algorithm;
+
+import java.util.Scanner;
+
+// 두 정수 A와 B를 입력받은 다음, AxB를 출력하는 프로그램을 작성하시오.
+public class Backjoon_10998 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int a,b;
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(a*b);
+    }
+}
