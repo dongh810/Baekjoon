d1c6dffe456d7c213293e8e6de150ff65e4130ba
동동 <baek0810@naver.com>
2024-04-23 15:24:16 +0900

    아스키 코드

---
+++ src/Baekjoon_Algorithm/Baekjoon_11654.java
@@ -0,0 +1,13 @@
+package Baekjoon_Algorithm;
+
+import java.util.Scanner;
+
+public class Baekjoon_11654 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char c = in.nextLine().charAt(0);
+        int num = (int)c;
+
+        System.out.println(num);
+    }
+}
