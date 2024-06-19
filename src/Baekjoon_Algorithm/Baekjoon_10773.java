a91a05d47fe34021cbb04e61af57088055f53aed
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-06-19 21:36:42 +0900

    제로

---
+++ src/Baekjoon_Algorithm/Baekjoon_10773.java
@@ -0,0 +1,44 @@
+package Baekjoon_Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 입력받을 배열 numArray[] / 입력받을 개수 K
+// 입력받을 수 inputNum / 현재 위치 location
+
+public class Baekjoon_10773 {
+    public static BufferedReader br;
+    public static StringBuilder sb;
+    public static StringTokenizer st;
+    public static int K;
+    public static int[] numArray;
+    public static int inputNum;
+    public static int location;
+    public static int result;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        K = Integer.parseInt(br.readLine());
+        numArray = new int[K];
+        location = 0;
+        result = 0;
+        for (int i = 0; i < K; i++) {
+            inputNum = Integer.parseInt(br.readLine());
+
+            if(inputNum == 0 && i!=0) {
+                numArray[location-1] = 0;
+                location--;
+            } else {
+                numArray[location] = inputNum;
+                location++;
+            }
+        }
+        for (int i = 0; i < location; i++) {
+            result = result + numArray[i];
+        }
+        System.out.println(result);
+    }
+}
